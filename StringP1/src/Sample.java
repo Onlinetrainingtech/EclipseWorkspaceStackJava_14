61901986d631b26ba741d93f8842977266f190b9
OnlineTrainer <mohamedtechdata@gmail.com>
2022-09-14 22:21:33 +0530

    14-09-2022

---
+++ StringP1/src/Sample.java
@@ -0,0 +1,17 @@
+
+public class Sample {
+
+	public static void main(String[] args) {
+		
+		String s1=new String("Welcome");
+		System.out.println("Your value is::"+s1);
+		String s2="Hello";
+		System.out.println("Your value is::"+s2);
+		
+		System.out.println("length::"+s1.length());
+		System.out.println("UpperCase"+s1.toUpperCase());
+		System.out.println("Compare s1 and s2"+s1.compareTo(s2));
+
+	}
+
+}
---
+++ StringP2/src/Sample.java
@@ -0,0 +1,13 @@
+
+public class Sample {
+
+	public static void main(String[] args) {
+		
+		StringBuilder sb=new StringBuilder("Welcometostring");
+		System.out.println("Your value is::"+sb);
+		System.out.println("Appending::"+sb.append("Hai"));
+		
+
+	}
+
+}
