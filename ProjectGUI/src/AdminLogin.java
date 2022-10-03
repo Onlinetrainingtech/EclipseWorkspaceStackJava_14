0300446a91e56ee062c657ffe6eb29901f0b5727
OnlineTrainer <mohamedtechdata@gmail.com>
2022-10-03 22:20:56 +0530

    03-10-2022

--- ProjectGUI/src/AdminLogin.java
+++ ProjectGUI/src/AdminLogin.java
@@ -4,8 +4,16 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.awt.event.ActionEvent;
 
 public class AdminLogin extends JFrame {
 
@@ -68,7 +76,44 @@ public class AdminLogin extends JFrame {
 		panel.add(textField_1);
 		textField_1.setColumns(10);
 		
-		JButton btnNewButton = new JButton("Submit");
+		final JButton btnNewButton = new JButton("Submit");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+				try
+				{
+					String u1=textField.getText();
+					String p1=textField_1.getText();
+					String str1="select * from admin";
+					Class.forName("org.h2.Driver");
+					Connection conn=DriverManager.getConnection("jdbc:h2:tcp://localhost/~/javab14","sa","");
+					Statement stmt=conn.createStatement();
+					ResultSet rs=stmt.executeQuery(str1);
+					rs.next();
+					String uname=rs.getString(1);
+					String pass=rs.getString(2);
+					if(u1.equals(uname)&&p1.equals(pass))
+					{
+						JOptionPane.showMessageDialog(btnNewButton,"LoginSucess!!!");
+						new AdminHomePage().setVisible(true);
+					}
+					else
+					{
+						JOptionPane.showMessageDialog(btnNewButton,"LoginFail!!!");
+					}
+					
+					
+					
+				}
+				catch(SQLException t)
+				{
+					System.out.println(t);
+				} catch (ClassNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} 
+			}
+		});
 		btnNewButton.setBounds(57, 176, 89, 23);
 		panel.add(btnNewButton);
 		
